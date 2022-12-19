dedc4521dbc0d6655760621d7c7534ed9bd9767d
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-12-19 16:36:05 +0500

    the Header component is implemented

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import CTA from './CTA';
+import HeaderSocial from './HeaderSocial';
+
+import Me from '../../assets/me.png';
+
 import './Header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
+        <h1>Alex</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocial />
+        <div className="me">
+          <img src={Me} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;

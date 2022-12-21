3cf65f62650475b701c58a3df6dfe27ede2c1318
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-12-21 13:25:38 +0500

    implemented the Portfolio component

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import './Portfolio.css';
+import React from "react";
+import "./Portfolio.css";
+import portfolioData from "./portfolioData";
 
 const Portfolio = () => {
-  return <section id='portfolio'>
-    <h5>My Recent Work</h5>
-    <h2>Portfolio</h2>
-    <div className="container portfolio__container">
-      <article className="portfolio__item">
-        <div className="portfolio__item_image">
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className='btn' target="_blank">Github</a>
-          <a href="https://github.com" className='btn btn-primary' target="_blank">LiveDemo</a>
+      return <section id="portfolio">
+        <h5>My Recent Work</h5>
+        <h2>Portfolio</h2>
+        <div className="container portfolio__container">
+          {
+            portfolioData.map(({id, image, title, github, demo}) => {
+              return (
+                  <article className="portfolio__item" key={id}>
+                    <div className="portfolio__item_image">
+                      <img src={image} alt={`img${id}`}/>
+                    </div>
+                    <h3>{title}</h3>
+                    <div className="portfolio__item-cta">
+                      <a href={github} className="btn" rel="noopener">Github</a>
+                      <a href={demo} className="btn btn-primary" rel="noopener">LiveDemo</a>
+                    </div>
+                  </article>
+              );
+            })
+          }
         </div>
-      </article>
-    </div>
-  </section>;
-};
+      </section>;
+    }
+;
 
 export default Portfolio;
---
+++ src/components/Portfolio/portfolioData.js
@@ -0,0 +1,54 @@
+import pf1 from "./../../assets/portfolio1.jpg";
+import pf2 from "./../../assets/portfolio2.jpg";
+import pf3 from "./../../assets/portfolio3.jpg";
+import pf4 from "./../../assets/portfolio4.jpg";
+import pf5 from "./../../assets/portfolio5.png";
+import pf6 from "./../../assets/portfolio6.jpg";
+
+const portfolioData = [
+  {
+    id: 1,
+    image: pf1,
+    title: "Crypto Currency Dashboard & Financial Visualisation",
+    github: "https://github.com",
+    demo: "https://github.com"
+  },
+  {
+    id: 2,
+    image: pf2,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com",
+    demo: "https://github.com"
+  },
+  {
+    id: 3,
+    image: pf3,
+    title: "Figma dashboard UI kit for data design web apps",
+    github: "https://github.com",
+    demo: "https://github.com"
+  },
+  {
+    id: 4,
+    image: pf4,
+    title: "Maintaining tasks and tracking progress",
+    github: "https://github.com",
+    demo: "https://github.com"
+  },
+  {
+    id: 5,
+    image: pf5,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com",
+    demo: "https://github.com"
+  },
+  {
+    id: 6,
+    image: pf6,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com",
+    demo: "https://github.com"
+  },
+
+];
+
+export default portfolioData;

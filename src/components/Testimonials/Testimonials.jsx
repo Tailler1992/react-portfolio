b4db067d79e1a4c355fd10182e802750d6e9f814
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-12-21 13:27:10 +0500

    implemented the Testimonials component

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
-import './Testimonials.css';
+import React from "react";
+import "./Testimonials.css";
+import testimonialsData from "./testimonialsData";
+
+import {Pagination} from "swiper";
+import {Swiper, SwiperSlide} from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Testimonials = () => {
-  return <section id='testimonials'>Testimonials</section>;
+  return <section id="testimonials">
+    <h5>Review from clients</h5>
+    <h2>Testimonials</h2>
+    <Swiper
+        className="container testimonials__container"
+        modules={[ Pagination]}
+        pagination={{ clickable: true }}
+        spaceBetween={40}
+        slidesPerView={1}
+    >
+      {
+        testimonialsData.map(({avatar, name, review}, i) => {
+          return (
+              <SwiperSlide key={i} className="testimonial">
+                <div className="client__avatar">
+                  <img src={avatar} alt=""/>
+                </div>
+                <h5 className="client__name">{name}</h5>
+                <small className="client__review">{review}</small>
+              </SwiperSlide>
+          )
+        })
+      }
+    </Swiper>
+  </section>;
 };
 
 export default Testimonials;
---
+++ src/components/Testimonials/testimonialsData.js
@@ -0,0 +1,37 @@
+import avatar1 from "./../../assets/avatar1.jpg";
+import avatar2 from "./../../assets/avatar2.jpg";
+import avatar3 from "./../../assets/avatar3.jpg";
+import avatar4 from "./../../assets/avatar4.jpg";
+
+const testimonialsData = [
+  {
+    avatar: avatar1,
+    name: "Tina Snow",
+    review: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur iure +
+        iusto nihil rerum vel. Accusamus ad incidunt laborum magnam magni mollitia neque perspiciatis? Eius enim
+        molestiae officia quibusdam repudiandae vitae`
+  },
+  {
+    avatar: avatar2,
+    name: "Shatta Wale",
+    review: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur iure +
+        iusto nihil rerum vel. Accusamus ad incidunt laborum magnam magni mollitia neque perspiciatis? Eius enim
+        molestiae officia quibusdam repudiandae vitae`
+  },
+  {
+    avatar: avatar3,
+    name: "Kwame Despite",
+    review: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur iure +
+        iusto nihil rerum vel. Accusamus ad incidunt laborum magnam magni mollitia neque perspiciatis? Eius enim
+        molestiae officia quibusdam repudiandae vitae`
+  },
+  {
+    avatar: avatar4,
+    name: "Anna Brown",
+    review: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur iure +
+        iusto nihil rerum vel. Accusamus ad incidunt laborum magnam magni mollitia neque perspiciatis? Eius enim
+        molestiae officia quibusdam repudiandae vitae`
+  },
+];
+
+export default testimonialsData;
